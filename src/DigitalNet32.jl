d0b9737140682b7486e3c335e264489ba576ed5e
Philippe Blondeel <philippe.blondeel@kuleuven.be>
2021-12-03 09:29:48 +0100

    update

--- src/DigitalNet32.jl
+++ src/DigitalNet32.jl
@@ -57,37 +57,37 @@ end
 
 function DigitalNet32(s::Integer)
     s > 0 || throw(ArgumentError("number of dimensions s must be larger than 0"))
-    DigitalNet32(sobol_Cs,s,2^20)
+    DigitalNet32(sobol_Cs,s,2^50)
 end
 
 function DigitalNet64(s::Integer)
     s > 0 || throw(ArgumentError("number of dimensions s must be larger than 0"))
 #    print(@__DIR__())
-    DigitalNet64(sobol_Cs64,s,2^20)
+    DigitalNet64(sobol_Cs64,s,2^50)
 end
 
 function DigitalNet64InterlacedTwo(s::Integer)
     s > 0 || throw(ArgumentError("number of dimensions s must be larger than 0"))
 #    print(@__DIR__())
-    DigitalNet64(sobol_alpha2_Bs53,s,2^20)
+    DigitalNet64(sobol_alpha2_Bs53,s,2^50)
 end
 
 function DigitalNet64InterlacedThree(s::Integer)
     s > 0 || throw(ArgumentError("number of dimensions s must be larger than 0"))
 #    print(@__DIR__())
-    DigitalNet64(sobol_alpha3_Bs53,s,2^20)
+    DigitalNet64(sobol_alpha3_Bs53,s,2^50)
 end
 
 function DigitalNet64InterlacedThree64(s::Integer)
     s > 0 || throw(ArgumentError("number of dimensions s must be larger than 0"))
 #    print(@__DIR__())
-    DigitalNet64(sobol_alpha3_Bs64,s,2^20)
+    DigitalNet64(sobol_alpha3_Bs64,s,2^50)
 end
 
 function DigitalNet64InterlacedFour(s::Integer)
     s > 0 || throw(ArgumentError("number of dimensions s must be larger than 0"))
 #    print(@__DIR__())
-    DigitalNet64(sobol_alpha4_Bs53,s,2^20)
+    DigitalNet64(sobol_alpha4_Bs53,s,2^50)
 end
 
 
@@ -95,7 +95,7 @@ end
 function DigitalNet64InterlacedFive(s::Integer)
     s > 0 || throw(ArgumentError("number of dimensions s must be larger than 0"))
 #    print(@__DIR__())
-    DigitalNet64(sobol_alpha5_Bs53,s,2^20)
+    DigitalNet64(sobol_alpha5_Bs53,s,2^50)
 end#reversebits(n::U) where {U<:Unsigned} = parse(U, reverse(bitstring(n)), base=2)
 
 
